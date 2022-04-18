ab8b97b31a7a283e26b8c016992e57202f4be662
Real-Bird <marbil815@gmail.com>
2022-04-18 22:14:44 +0900

    22-04-18 tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-blue-200">it work</h1>
+    </div>
+  );
 };
 
 export default Home;
