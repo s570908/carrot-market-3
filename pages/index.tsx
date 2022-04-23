e52f406e782798d70031175439f7d63501bd58dd
Real-Bird <marbil815@gmail.com>
2022-04-24 00:35:26 +0900

    22-04-24 list marker

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,11 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 p-5">
-      <details className="select-none open:bg-indigo-400 open:text-white">
-        <summary className="cursor-pointer">What is my fav food</summary>
-        <span>피자</span>
-      </details>
+      <ul className="list-decimal marker:text-teal-500">
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+      </ul>
     </div>
   );
 };
