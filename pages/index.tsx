e5ddb6f3066302216752bfafe659de60034cd049
Real-Bird <marbil815@gmail.com>
2022-04-24 00:55:52 +0900

    22-04-24 first-letter, first-line

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 p-5">
-      <input
-        type="file"
-        className="file:hover: file:cursor-pointer file:rounded-xl file:border-0 file:bg-purple-400 file:px-5 file:text-white file:transition-colors file:hover:border file:hover:border-purple-400 file:hover:bg-white file:hover:text-purple-400"
-      />
+      <p className="first-letter:text-7xl first-line:hover:text-purple-400 ">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat
+        voluptatum quam quos aspernatur magni nobis quis velit eius nulla,
+        architecto beatae perspiciatis, deleniti obcaecati, ipsa ducimus
+        necessitatibus doloremque dolores debitis!
+      </p>
     </div>
   );
 };
