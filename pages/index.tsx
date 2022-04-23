47f1e6b4dd7dd32d01d1a4e49597a1be0af4cf8b
Real-Bird <marbil815@gmail.com>
2022-04-24 00:52:15 +0900

    22-04-24 file:hover:blabla

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,10 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 p-5">
-      <ul className="list-decimal marker:text-teal-500">
-        <li>hi</li>
-        <li>hi</li>
-        <li>hi</li>
-      </ul>
+      <input
+        type="file"
+        className="file:hover: file:cursor-pointer file:rounded-xl file:border-0 file:bg-purple-400 file:px-5 file:text-white file:transition-colors file:hover:border file:hover:border-purple-400 file:hover:bg-white file:hover:text-purple-400"
+      />
     </div>
   );
 };
