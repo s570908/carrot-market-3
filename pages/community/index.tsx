a2e2906a6fc5834f6bff42501106322e2ac4e3ac
Real-Bird <marbil815@gmail.com>
2022-04-28 22:34:07 +0900

    22-04-28 community, chats, profile

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -5,7 +5,7 @@ const Community: NextPage = () => {
     <div className="space-y-8 px-4 py-16">
       {[1, 2, 3, 4, 5].map((_, i) => (
         <div key={i} className="flex flex-col items-start">
-          <span className="fonme flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs text-gray-800">
+          <span className="flex items-center rounded-full bg-gray-100 px-2.5 py-0.5 text-xs font-medium text-gray-800">
             동네질문
           </span>
           <div className="mt-2 text-gray-700">
