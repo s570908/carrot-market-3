6b8261e30461586105cdf7ccc994cc581d17607a
Real-Bird <marbil815@gmail.com>
2022-05-02 22:20:39 +0900

    22-05-02 floating-btn, push shcema

--- components/floating-button.tsx
+++ components/floating-button.tsx
@@ -9,7 +9,7 @@ interface FloatingButton {
 export default function FloatingButton({ children, href }: FloatingButton) {
   return (
     <Link href={href}>
-      <a className="fixed bottom-24 right-5 flex aspect-square w-14 cursor-pointer  items-center justify-center rounded-full border-0 border-transparent bg-orange-400 text-white shadow-xl transition-colors hover:bg-orange-500">
+      <a className="fixed bottom-24 right-5 flex aspect-square w-14 cursor-pointer items-center justify-center  rounded-full border-0 border-transparent bg-orange-400 text-white shadow-xl transition-colors hover:bg-orange-500 sm:sticky sm:translate-x-[32rem]">
         {children}
       </a>
     </Link>
