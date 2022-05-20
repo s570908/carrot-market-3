66a547c68490be424416a62c8626f9cffd3fa47b
Real-Bird <marbil815@gmail.com>
2022-05-20 22:03:25 +0900

    22-05-20 twilio

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,12 +1,18 @@
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
-import { prisma } from "@prisma/client";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
-  const payload = Math.floor(10000 + Math.random() * 900000) + "";
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
   const token = await client.token.create({
     data: {
       payload,
@@ -23,8 +29,17 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
-  console.log(token);
-  return res.status(200).end();
+  if (phone) {
+    const messages = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!, // 정상적으로는 req.phone을 써야 함
+      body: `당신의 로그인 토큰은 ${payload}입니다.`,
+    });
+    console.log(messages);
+  }
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
