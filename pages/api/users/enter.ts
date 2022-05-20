52939eed0f67769c44913cd1070e455d622a6f36
Real-Bird <marbil815@gmail.com>
2022-05-20 16:01:29 +0900

    22-05-20 #9.1 prisma upsert

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -5,43 +5,17 @@ import { prisma } from "@prisma/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  let user;
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("찾았다");
-    if (!user) {
-      console.log("못 찾았다. 새로 만들어라.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log("찾았다");
-    if (!user) {
-      console.log("못 찾았다. 새로 만들어라.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  }
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
   return res.status(200).end();
 }
 
