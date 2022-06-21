a8490327c47a5dd4a4a488cbf186daa22a16f22c
Real-Bird <marbil815@gmail.com>
2022-06-21 18:49:08 +0900

    22-06-21 #19.5 Script Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,19 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className="mx-auto w-full max-w-xl">
         <Component {...pageProps} />
       </div>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        onLoad={() => {
+          window.fbAsyncInit = function () {
+            FB.init({
+              appId: "your-app-id",
+              autoLogAppEvents: true,
+              xfbml: true,
+              version: "v13.0",
+            });
+          };
+        }}
+      />
     </SWRConfig>
   );
 }
