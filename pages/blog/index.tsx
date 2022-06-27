c32c99b77ada07092218653c9cac6cb1621fe6c3
Real-Bird <marbil815@gmail.com>
2022-06-27 19:12:50 +0900

    22-06-27 #19.11~12 getStaticPaths

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -2,11 +2,13 @@ import Layout from "@components/layout";
 import { readdirSync, readFileSync } from "fs";
 import matter from "gray-matter";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Post {
   title: string;
   date: string;
   category: string;
+  slug: string;
 }
 
 const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
@@ -16,13 +18,17 @@ const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
         Latest Posts:
       </h1>
       {posts?.map((item, idx) => (
-        <div key={idx} className="mb-5">
-          <span className="text-lg text-red-500">{item.title}</span>
-          <div>
-            <span>
-              {item.date} / {item.category}
-            </span>
-          </div>
+        <div key={idx} className="mb-5 px-10">
+          <Link href={`/blog/${item.slug}`}>
+            <a>
+              <span className="text-lg text-red-500">{item.title}</span>
+              <div>
+                <span>
+                  {item.date} / {item.category}
+                </span>
+              </div>
+            </a>
+          </Link>
         </div>
       ))}
     </Layout>
@@ -32,11 +38,12 @@ const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
 export default Blog;
 
 export async function getStaticProps() {
-  const posts = readdirSync("./posts").map((file) => {
+  const blogPosts = readdirSync("./posts").map((file) => {
     const content = readFileSync(`./posts/${file}`, "utf-8");
-    return matter(content).data;
+    const [slug, _] = file.split(".");
+    return { ...matter(content).data, slug };
   });
   return {
-    props: { posts },
+    props: { posts: blogPosts.reverse() },
   };
 }
---
+++ posts/03-third-make-file.md
@@ -0,0 +1,8 @@
+---
+title: Third File
+date: 2022.06.24
+category: FS
+---
+# my third file
+
+congratulations!
