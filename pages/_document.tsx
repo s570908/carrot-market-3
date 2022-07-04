d97e36661442f262a344de850865dc532f6baa4f
Real-Bird <marbil815@gmail.com>
2022-07-04 21:35:00 +0900

    22-07-04 #21.3

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,18 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-class CustomDocument extends Document {
-  render(): JSX.Element {
-    return (
-      <Html lang="ko">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function MyDocument() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default CustomDocument;
